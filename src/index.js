287671265f6a26ba022c501568bacdc794c4af0c
sahildotexe <sahilkaling@gmail.com>
2023-10-26 23:07:23 +0530

    added email

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+import express from "express";
+import axios from "axios";
+import cheerio from "cheerio";
+import { Resend } from "resend";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-app.get('/scrape', async (req, res) => {
-    try {
-      const urlToScrape = "https://careers.zerodha.com/"; 
-      const response = await axios.get(urlToScrape);
-      const $ = cheerio.load(response.data);
-      const my5Divs = $('div.result');
-      const jobPostings = [];
-      const keywordsToCheck = ["developer", "engineer", "SDE"];
+app.get("/scrape", async (req, res) => {
+  try {
+    const urlToScrape = "https://careers.zerodha.com/";
+    const response = await axios.get(urlToScrape);
+    const $ = cheerio.load(response.data);
+    const my5Divs = $("div.result");
+    const jobPostings = [];
+    const keywordsToCheck = ["developer", "engineer", "SDE"];
 
-      my5Divs.each((index, element) => {
-        const h3Content = $(element).find('h3').text(); 
-        if (keywordsToCheck.some(keyword => h3Content.toLowerCase().includes(keyword.toLowerCase()))) {
-            jobPostings.push(h3Content);
-        }
+    my5Divs.each((index, element) => {
+      const h3Content = $(element).find("h3").text();
+      if (
+        keywordsToCheck.some((keyword) =>
+          h3Content.toLowerCase().includes(keyword.toLowerCase())
+        )
+      ) {
+        jobPostings.push(h3Content);
+      }
+    });
+
+    if (jobPostings.length > 0) {
+      await resend.emails.send({
+        from: "Zerodha-Job-Tracker <onboarding@resend.dev>",
+        to: ["sahilkaling@gmail.com"],
+        subject: "Yoo Zerodha has new job openings!",
+        text: "Check out the new job openings at Zerodha: https://careers.zerodha.com/",
       });
-      
-      res.json({ jobPostings });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
     }
-  });
+
+    res.json({ jobPostings });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
